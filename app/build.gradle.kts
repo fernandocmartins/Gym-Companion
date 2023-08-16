099886ebb3e9dc221b9f75ece7b15e86f71c3a47
Fernando <fernandocsrmartins@gmail.com>
2023-08-16 19:17:01 -0300

    add viewBinding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,8 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        viewBinding.isEnabled = true
     }
 
     buildTypes {
@@ -37,7 +39,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- app/src/main/java/com/fernando/gymcompanion/MainActivity.kt
+++ app/src/main/java/com/fernando/gymcompanion/MainActivity.kt
@@ -2,10 +2,13 @@ package com.fernando.gymcompanion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fernando.gymcompanion.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding  by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
